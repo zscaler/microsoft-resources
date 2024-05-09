4ad91818af5a51be8aaaedea5b0bb452518dabc7
Paul Lopez <prlopez@gmail.com>
2024-05-09 10:10:37 +1000

    added additional fields in web logs

--- demo-resources/configuring-microsoft-sentinel-for-lss-with-ama/modify-dcr.ps1
+++ demo-resources/configuring-microsoft-sentinel-for-lss-with-ama/modify-dcr.ps1
@@ -13,17 +13,17 @@ $DCR.Content | ConvertFrom-Json | ConvertTo-Json -Depth 20 | Out-File -FilePath 
 $DCRContent = Get-Content $FilePath -Raw
 
 # Define the new properties to insert
-$newProperties = ', "transformKql": "source | where SyslogMessage contains \"PrivateSE\" or SyslogMessage contains \"MicroTenantID\" or SyslogMessage contains \"ConnectionID\" or SyslogMessage contains \"requestID\" | extend Message=SyslogMessage", "outputStream": "Custom-ZPA_CL"'
+#$newProperties = ', "transformKql": "source | where SyslogMessage contains \"PrivateSE\" or SyslogMessage contains \"MicroTenantID\" or SyslogMessage contains \"ConnectionID\" or SyslogMessage contains \"requestID\" | extend Message=SyslogMessage", "outputStream": "Custom-ZPA_CL"'
 
 # Adjust the pattern to include lookahead for proper placement after the destinations array
-$pattern = '(?<=("destinations": \[[^\]]*\]))'
+#$pattern = '(?<=("destinations": \[[^\]]*\]))'
 
 # Use the -replace operator to correctly add the new properties after the destinations array
-$DCRContent = $DCRContent -replace $pattern, "$newProperties"
+#$DCRContent = $DCRContent -replace $pattern, "$newProperties"
 
 
-$DCRContent | Set-Content -Path $FilePath
+#$DCRContent | Set-Content -Path $FilePath
 
 ### Upload DCR
-$UpdatedDCRContent = Get-Content $FilePath -Raw
-Invoke-AzRestMethod -Path ("$ResourceId"+"?api-version=2021-09-01-preview") -Method PUT -Payload $UpdatedDCRContent
+#$UpdatedDCRContent = Get-Content $FilePath -Raw
+#Invoke-AzRestMethod -Path ("$ResourceId"+"?api-version=2021-09-01-preview") -Method PUT -Payload $UpdatedDCRContent
